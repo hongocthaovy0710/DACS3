2ecff8cd2fe90e6c12657eaaee2c52d301201d04
nhungdth136 <161338577+nhungdth136@users.noreply.github.com>
2025-04-20 17:16:52 +0700

    xong vd4

---
+++ app/src/main/java/com/example/dacs3/Fragment/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.dacs3.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.dacs3.R
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+
+    }
+}
