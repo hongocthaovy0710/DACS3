110bfa64f83c84182ebec3598e85c49aeb345842
hongocthaovy0710 <hongocthaovy66@gmail.com>
2025-04-24 22:00:28 +0700

    xong vd9

--- app/src/main/java/com/example/dacs3/MainActivity.kt
+++ app/src/main/java/com/example/dacs3/MainActivity.kt
@@ -8,14 +8,21 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.dacs3.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+       setContentView(binding.root)
         var NavController = findNavController(R.id.fragmentContainerView)
         var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener {
+            val bottomSheetDialog = Notifaction_Bottom_Fragment()
+            bottomSheetDialog.show(supportFragmentManager, "Test")
+        }
     }
 }
